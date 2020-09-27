8da7b1c162146acaffeed7b7c1dadb84fd541176
Evandro Ribeiro <ribeiroevandro@live.com>
2020-09-27 04:37:05 -0300

    chore: add test for create products

---
+++ src/pages/product/index.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+import Layout from '~/components/Layout'
+
+const importProducts = async () => {
+
+  const markdownFiles = require
+    .context('~/content/products', false, /\.md$/)
+    .keys()
+    .map((relativePath) => relativePath.substring(2))
+
+  return Promise.all(
+    markdownFiles.map(async (path) => {
+      const markdown = await import(`~/content/products/${path}`)
+      return { ...markdown, slug: path.substring(0, path.length - 3) }
+    })
+  )
+}
+
+const Product = ({ productsList }) => (
+  <Layout>
+    {productsList.map((item) => (
+      <div key={item.slug} className="item">
+        <Link href="/products/item/[slug]" as={`/products/item/${item.slug}`}>
+          <a>
+            <img src={item.attributes.thumbnail} />
+            <h2>{item.attributes.title}</h2>
+          </a>
+        </Link>
+      </div>
+    ))}
+    <style jsx>{`
+      .post {
+        text-align: center;
+      }
+      img {
+        max-width: 100%;
+        max-height: 300px;
+      }
+    `}</style>
+  </Layout>
+)
+
+export async function getStaticProps() {
+  const productsList = await importProducts()
+
+  return {
+    props: {
+      productsList,
+    },
+  }
+}
+
+export default Product
---
+++ src/pages/product/item/[slug].tsx
@@ -0,0 +1,59 @@
+import fs from 'fs'
+import path from 'path'
+import Layout from '~/components/Layout'
+
+const Item = ({ blogpost }) => {
+  if (!blogpost) return <div>not found</div>
+
+  const { html, attributes } = blogpost
+
+  return (
+    <Layout>
+      <article>
+        <h1>{attributes.title}</h1>
+        <img src={attributes.thumbnail} />
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </article>
+      <style jsx>{`
+        article {
+          margin: 0 auto;
+        }
+        h1 {
+          text-align: center;
+        }
+      `}</style>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = fs
+    .readdirSync(path.join(process.cwd(), 'src', 'content/products'))
+    .map((blogName) => {
+      const trimmedName = blogName.substring(0, blogName.length - 3)
+      return {
+        params: { slug: trimmedName },
+      }
+    })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params
+
+  const blogpost = await import(`~/content/products/${slug}.md`).catch(
+    () => null
+  )
+
+  return {
+    props: {
+      blogpost: blogpost.default,
+    },
+  }
+}
+
+export default Item
