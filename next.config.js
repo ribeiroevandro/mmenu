5ee73df9d9b62a7b382118a9b33de01c7602ffa2
Evandro Ribeiro <ribeiroevandro@live.com>
2020-09-27 16:29:25 -0300

    chore: updates

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@ const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
+const sitemap = require('nextjs-sitemap-generator');
+
+sitemap({
+  baseUrl: isProd ? 'https://mmenu-cms.netlify.app' : 'http://localhost:3000',
+  pagesDirectory: __dirname + "/src/pages",
+  targetDirectory : 'public/static/'
+});
 
 module.exports = withPlugins(
   [
