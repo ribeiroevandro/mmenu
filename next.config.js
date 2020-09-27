6622045dae9868993e36910789630a488bf0ec9e
Evandro Ribeiro <ribeiroevandro@live.com>
2020-09-27 16:40:27 -0300

    chore: updates

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const sitemap = require('nextjs-sitemap-generator');
 sitemap({
   baseUrl: isProd ? 'https://mmenu-cms.netlify.app' : 'http://localhost:3000',
   pagesDirectory: __dirname + "/src/pages",
+  pagesDirectory: __dirname + "/.next/server/pages",
   targetDirectory : 'public/static/'
 });
 
