34c7f985ff0e7c037c45d38ba05f0e80eac2006b
Evandro Ribeiro <ribeiroevandro@live.com>
2020-09-27 11:22:26 -0300

    chore: updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Head from 'next/head'
-import { NextSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 
 import GlobalStyle from '../styles/global.styles'
@@ -12,26 +11,6 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <meta name="theme-color" content="#121421" />
       </Head>
-      <NextSeo
-        title="Meu Menu"
-        description="O jeito mais fácil de pedir delivery de comida. A maior lista de restaurantes online em sua cidade. Faça seu pedido pela internet e receba em casa."
-        canonical="https://mmenu-cms.netlify.app/"
-        openGraph={{
-          url: 'https://mmenu-cms.netlify.app/',
-          title:
-            'Meu Menu',
-          description:
-            'O jeito mais fácil de pedir delivery de comida. A maior lista de restaurantes online em sua cidade. Faça seu pedido pela internet e receba em casa.',
-          images: [{ url: 'https://devspace.app/img/cover.png' }],
-          site_name: 'DevSpace',
-          locale: 'pt_BR'
-        }}
-        twitter={{
-          handle: '@ribeiroevandro',
-          site: '@site',
-          cardType: 'summary_large_image'
-        }}
-      />
       <GlobalStyle />
       <AppProvider>
         <Component {...pageProps} />
