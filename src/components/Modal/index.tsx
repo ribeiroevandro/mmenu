9a7194f4cd7ed77abf388f6fbb131a1cdfc29800
Evandro Ribeiro <ribeiroevandro@live.com>
2020-09-28 04:43:11 -0300

    chore: updates

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,23 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { IconBaseProps } from 'react-icons';
 
-
 import * as S from './styles';
-
-interface ProductItemProps {
-  slug: string;
-  attributes: {
-    title: string;
-    date: string;
-    thumbnail_featured: string;
-    price: number;
-    description: string;
-  }
-}
-
-interface Bizarro {
-
-}
 
 interface ModalProps {
   isOpen: boolean;
